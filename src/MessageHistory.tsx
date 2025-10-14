e8945ddd6f426c215d7c0830def4f0f944ea6ad8
Linus Mähler <linus.mahler@realsprint.com>
2025-10-14 13:09:50 +0200

    Messagehistory takes up the remaining height of the left pane

--- src/MessageHistory.tsx
+++ src/MessageHistory.tsx
@@ -18,7 +18,7 @@ export const MessageHistory = ({ messages }: MessageHistoryProps) => {
   const [prettyPrint, setPrettyPrint] = useState<boolean>(true);
   const [maxMessages, setMaxMessages] = useState<number>(100);
   return (
-    <Box component="section" className="text-wrap break-words">
+    <Box component="section" className="text-wrap break-words h-full flexcol">
       <h2 className="text-base font-bold mb-3">Messages</h2>
       <FormControlLabel
         control={<Switch />}
@@ -42,7 +42,7 @@ export const MessageHistory = ({ messages }: MessageHistoryProps) => {
         label="Message history"
         onChange={(e) => setMaxMessages(Number.parseInt(e.target.value))}
       />
-      <div className="overflow-y-auto max-h-[calc(63vh-80px)] px-1">
+      <div className="flex-1 overflow-y-auto px-1">
         {messages.slice(0, maxMessages).map((message, idx) => (
           <Paper className="my-2 text-xs" elevation={3} key={idx}>
             <WebsocketMessage message={message} prettyPrint={prettyPrint} />
